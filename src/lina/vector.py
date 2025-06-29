dbc00c3124960638dbef465f519e01f23b97d441
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-29 10:16:45 +0300

    vec3

--- src/lina/vector.py
+++ src/lina/vector.py
@@ -28,3 +28,24 @@ class Vector2D[T]:
 
     def __truediv__(self, other: T):
         return Vector2D(self.x / other, self.y / other)
+
+
+@dataclass(frozen=True)
+class Vector3D[T]:
+    """Ход игрока"""
+
+    x: T
+    y: T
+    z: T
+
+    def __add__(self, other: Vector3D[T]):
+        return Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other: Vector3D[T]):
+        return Vector3D(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, other: T):
+        return Vector3D(self.x * other, self.y * other, self.z * other)
+
+    def __truediv__(self, other: T):
+        return Vector3D(self.x / other, self.y / other, self.z / other)
