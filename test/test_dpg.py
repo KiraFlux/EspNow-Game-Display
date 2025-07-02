db27966da85479d4bd3cac14d52ab155bd421d55
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-07-02 19:18:58 +0300

    upd

--- test/test_dpg.py
+++ test/test_dpg.py
@@ -3,22 +3,41 @@ from __future__ import annotations
 from dpg_ui.core.app import App
 from dpg_ui.impl.button import Button
 from dpg_ui.impl.container.box import HBox
+from dpg_ui.impl.container.tab import Tab
+from dpg_ui.impl.container.tab import TabBar
 from dpg_ui.impl.container.window import Window
 from dpg_ui.impl.display.text import DisplayText
 from dpg_ui.impl.input.text import InputText
 from dpg_ui.impl.slider.float_ import FloatSlider
 
+tab_bar = (
+    TabBar(_reorderable=True)
+    .add(
+        Tab("Tab 1")
+        .add(
+            HBox()
+            .add(Button("Button"))
+            .add(Button("1234567"))
+        )
+    )
+    .add(
+        Tab("2")
+        .add(InputText("string", print))
+        .add(FloatSlider("float", _value_default=0.123, _interval_max=10, _interval_min=-10))
+    )
+)
+
 __w = (
     Window("Text window")
-    .add(DisplayText(_value_default="text"))
     .add(
         HBox()
-        .add(Button("Button"))
-        .add(Button("Button"))
+        .add(tab_bar)
+        .add(DisplayText(_value_default="text"))
     )
-    .add(InputText("string", print))
-    .add(FloatSlider("float", _value_default=0.123, _interval_max=10, _interval_min=-10))
-    .add(Window("SHoo", _auto_size=False).add(Button("lol")))
+    .add(
+        Window("SHoo", _auto_size=False)
+        .add(Button("lol"))
+    )
 )
 
 App(__w).run("title", 1280, 720)
